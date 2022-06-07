735698f93b3ef8d86ae972cddd2ed1d279923d7a
Adroaldo <hygino@msn.com>
2022-06-07 20:23:20 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp html', 'https://cdn.pixabay.com/photo/2020/05/31/09/40/online-course-5242018_1280.jpg', 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/education-school-logo-design-template-680c4e52db7c2ba87ff682b4730a44ee_screen.jpg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00Z', 1);
